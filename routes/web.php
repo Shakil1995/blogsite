037b1fcc46f319670b47b430f1135488484c266b
shakil1995 <na.shakil1995@gmail.com>
2021-11-06 01:37:39 +0600

    auth added

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/', function () {
+//     return view('layouts.app');
+// });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/check',function(){
+echo "check done";
+})->name('check')->middleware(('verified'));
